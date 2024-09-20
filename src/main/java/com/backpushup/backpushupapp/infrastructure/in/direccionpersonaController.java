8f530d267f4bdbaab839e6543bea037505cf314a
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-09-20 16:26:18 -0500

    feat: :sparkles: FIx
    Fix names controllers

--- src/main/java/com/backpushup/backpushupapp/infrastructure/in/direccionpersonaController.java
+++ src/main/java/com/backpushup/backpushupapp/infrastructure/in/direccionpersonaController.java
@@ -7,7 +7,14 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.backpushup.backpushupapp.application.DireccionpersonaService;
 import com.backpushup.backpushupapp.domain.Direccionpersona;
@@ -56,6 +63,7 @@ public class DireccionpersonaController {
         response.put("message", "DireccionPersona actualizado correctamente");
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+    
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDireccionPersona(@PathVariable Long id) {
